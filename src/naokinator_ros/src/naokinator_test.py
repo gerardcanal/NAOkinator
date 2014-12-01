86c5fbca2dbe1cdaa491e0acb24a09d788b10a71
Gerard Canal <grar.knal@gmail.com>
2014-12-01 16:50:23 +0100

    Working on test

--- src/naokinator_ros/src/naokinator_test.py
+++ src/naokinator_ros/src/naokinator_test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import rospy
+import sys
 from naokinator_ros.srv import akinator_srv
 from std_msgs.msg import String
 from std_srvs.srv import Empty
@@ -7,12 +8,16 @@ from nao_msgs.msg import WordRecognized
 ## FIXME words must be in the robot to be able to recognize something
 
 word = None
+yes_words = ['yes', 'si', 'se', 'yea', 'yeah', 'see', 'sea', 'yi', 'yees', 'probably', 'maybe']
+no_words = ['no', 'nu', 'mo','mu','nou', 'now', 'nope']
+
 def callback(data):
     global word
     maxc = -1
+    print "Recognized words:", zip(data.words, data.confidence_values)
     for w, c in zip(data.words, data.confidence_values):
         if c > maxc:
-            word = w
+            word = 'yes' if w in yes_words else 'no'
             maxc = c
 
 if __name__ == "__main__":
@@ -29,11 +34,13 @@ if __name__ == "__main__":
     while not resp.is_guess:
         startrec()
         pub.publish(resp.question_guess_response)
+        sys.stdout.write(resp.question_guess_response+' ')
         while word is None:
-            pass
-        print resp.question_guess_response+' '+word
+            if rospy.is_shutdown():
+                sys.exit(-1)
+        stoprec()
+        print word
         resp = naokinatorsrv(word)
         word = None
-        stoprec()
     pub.publish('I am done! You were thinking about '+resp.question_guess_response)
     print 'I am done! You were thinking about '+resp.question_guess_response
