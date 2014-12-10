342f576b1151ae2076de458eba50e41baad2869b
Dani <naeinnashesz@gmail.com>
2014-12-10 13:24:45 -0600

    AkinatorStateMachine done. AkinatorGame fixed Akinator Answer in SM format using NAO-UPC nao_smach_utils

--- src/naokinator_ros/src/smach_AkinatorAnswer.py
+++ src/naokinator_ros/src/smach_AkinatorAnswer.py
@@ -2,70 +2,46 @@ __author__ = 'dani'
 
 import rospy
 import sys
-from smach import StateMachine
+from smach import StateMachine,State
 
 from std_msgs.msg import String
 from std_srvs.srv import Empty
 from naoqi_msgs.msg import WordRecognized
+
 from nao_smach_utils.execute_speechgesture_state import SpeechGesture
+from nao_smach_utils.speech_recognition_states import StartRecognitionState,StopRecognitionState,GetRecognizedWord
 
 class AkinatorAnswer(StateMachine):
     def __init__(self):
         StateMachine.__init__(self, outcomes=['succeeded', 'aborted', 'preempted'])
         with self:
-            # TODO integrate movements, speech and retrieval from akinator service
+            '''
             StateMachine.add('Answer',
                              Answer(),
                              transitions={'succeeded':'THINKING', 'aborted':'aborted'},
                              remapping={'text':'text'}
             )
+            '''
+            StateMachine.add('START_LISTEN',
+                             StartRecognitionState(),
+                             transitions={'succeeded':'ANSWER_DETECTION'}
+            )
+            StateMachine.add('ANSWER_DETECTION',
+                             GetRecognizedWord(timeout=30),
+                             transitions={'succeeded':'THINKING',
+                                          'timeouted':'STOP_LISTEN_ABORTED'},
+                             remapping={'recognized_words':'text'}
+            )
 
             StateMachine.add('THINKING',
                              SpeechGesture(text='Let me see...', behavior_name='Thinking1'),
-                             transitions={'succeeded':'succeeded'})
+                             transitions={'succeeded':'STOP_LISTEN_SUCCEEDED'})
 
-
-word = None
-class Answer(smach.State):
-    def __init__(self, timeout = None):
-        import random
-        self.startrec = rospy.ServiceProxy('/start_recognition', Empty)
-        self.stoprec = rospy.ServiceProxy('/stop_recognition', Empty)
-        self.pub = rospy.Publisher('/speech', String)
-        rospy.Subscriber("/word_recognized", WordRecognized, self.callback)
-
-        if timeout is None:
-            self._timeout = random.randint(1,15) + 25
-        else:
-            self._timeout = timeout
-
-
-    def callback(data):
-        ## FIXME words must be in the robot to be able to recognize something
-        yes_words = ['yes', 'si', 'se', 'yea', 'yeah', 'see', 'sea', 'yi', 'yees', 'probably', 'maybe']
-        no_words = ['no', 'nu', 'mo','mu','nou', 'now', 'nope']
-
-        maxc = -1
-        print "Recognized words:", zip(data.words, data.confidence_values)
-        for w, c in zip(data.words, data.confidence_values):
-            if c > maxc:
-                word = 'yes' if w in yes_words else 'no'
-                maxc = c
-
-    def execute(self, userdata):
-        global word
-        self.startrec()
-        self.pub.publish(userdata.text)
-        sys.stdout.write(userdata.text+' ')
-
-        startT = rospy.Time.now()
-        timeout = False
-        while not (timeout or (word is None)):
-            timeout = (rospy.Time.now()-startT) > self._timeout
-            if rospy.is_shutdown():
-                sys.exit(-1)
-
-        self.stoprec()
-        print word
-        word = None
-        self.pub.unregister()
+            StateMachine.add('STOP_LISTEN_SUCCEEDED',
+                             StopRecognitionState(),
+                             transitions={'succeeded':'succeeded'}
+            )
+            StateMachine.add('STOP_LISTEN_ABORTED',
+                             StopRecognitionState(),
+                             transitions={'succeeded':'aborted'}
+            )
--- src/naokinator_ros/src/smach_akinatorStateMachine.py
+++ src/naokinator_ros/src/smach_akinatorStateMachine.py
@@ -2,7 +2,7 @@ __author__ = 'dani'
 #!/usr/bin/env python
 import rospy
 import sys
-from smach import StateMachine, State, SimpleActionState
+from smach import StateMachine, State, SimpleActionState, CBState
 
 from naokinator_ros.srv import akinator_srv
 from std_msgs.msg import String
@@ -18,39 +18,68 @@ class AkinatorGame(StateMachine):
         StateMachine.__init__(self, outcomes=['succeeded', 'aborted', 'preempted'])
         self.userdata.is_guess = False
         self.userdata.text = None
+        self.userdata.max_questions = 25
         self.userdata.n_questions = 0
+        self.userdata.max_repeats = 3
         self.userdata.n_repeats = 0
         with self:
             # TODO integrate movements, timeout, unsuscribing from /word_recognized and maximum questions to WIN/LOSE
             StateMachine.add('QUESTION',
                              AkinatorQuestion(),
-                             transitions={'succeeded':'ANSWER'},
+                             transitions={'succeeded':'GET_USER_ANSWER'},
                              remapping={'text':'text'}
             )
-            StateMachine.add('ANSWER',
+            StateMachine.add('GET_USER_ANSWER',
                              AkinatorAnswer(),
-                             transitions={'succeeded':'succeeded' if self.userdata.is_guess else 'QUESTIONSCOUNT',
+                             transitions={'succeeded':'ISGUESS',
                                           'aborted':'REPEAT'},
                              remapping={'text':'text', 'is_guess_out':'is_guess'}
             )
-            StateMachine.add('QUESTIONSCOUNT',
-                             CountdownController(25),
-                             transitions={'succeeded':'QUESTION','aborted':'succeeded'},
-                             remapping={'count':'n_questions'}
+
+            StateMachine.add('ISGUESS',
+                             CBState(self.guess_cb, input_keys=['is_guess'], outcomes=['finished', 'continue']),
+                             transitions={'continue':'QUESTIONCOUNT',
+                                          'finished':'succeeded'},
             )
+
+            StateMachine.add('QUESTIONSCOUNT',
+                             #CountdownController(self.userdata.max_questions),
+                             CBState(self.countdown_cb, input_keys=['n_questions','max_questions'], outcomes=['succeeded', 'aborted']),
+                             transitions={'succeeded':'QUESTION','aborted':'succeeded'},
+                             remapping={'count':'n_questions','max':'max_questions'}
+            )
+
             StateMachine.add('REPEAT',
                              SpeechGesture(text='I did not listen you. Can your repeat?', behavior_name='AskingAgain1'),
-                             transitions={'succeeded':'ANSWER'})
-            StateMachine.add('REPEATCOUNT',
-                             CountdownController(2),
-                             transitions={'succeeded':'QUESTION','aborted':'succeeded'},
-                             remapping={'count':'n_questions'})
-            StateMachine.add('REPEATRESET',
-                             Repeat_reset(2),
-                             transitions={'succeeded':'QUESTION','aborted':'succeeded'},
-                             remapping={'count':'n_questions'})
+                             transitions={'succeeded':'GET_USER_ANSWER'})
 
-class CountdownController(smach.State):
+            StateMachine.add('REPEATCOUNT',
+                             #CountdownController(2),
+                             CBState(self.countdown_cb, input_keys=['n_repeats','max_repeats'], outcomes=['succeeded', 'aborted']),
+                             transitions={'succeeded':'REPEATRESET','aborted':'succeeded'},
+                             remapping={'count':'n_repeats','max':'max_repeats'})
+
+            StateMachine.add('REPEATRESET',
+                             CBState(self.reset_cb, input_keys=['n_repeats'], outcomes=['succeeded']),
+                             transitions={'succeeded':'QUESTION'},
+                             remapping={'var':'n_repeats'})
+
+
+    def guess_cb(ud):
+        if ud.is_guess: return 'finished'
+        return 'continue'
+
+    def countdown_cb(ud):
+        ud.count = ud.count+1
+        if ud.counter < ud.max: return 'succeeded'
+        return 'aborted'
+
+    def reset_cb(ud):
+        ud.var = 0
+        return 'succeeded'
+
+'''
+class CountdownController(State):
     def __init__(self, max):
         self.max = max
 
@@ -60,3 +89,4 @@ class CountdownController(smach.State):
             return 'succeeded'
         else:
             return 'aborted'
+'''
